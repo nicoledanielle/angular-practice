087e6687d6309ccb074b1a660a908272b629d31c
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-04-12 12:44:53 -0700

    added firebase dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZchoeLQ1Os-J_-y1MaDk1G30KkGNHDHI",
+    authDomain: "o-shop-8fc2f.firebaseapp.com",
+    databaseURL: "https://o-shop-8fc2f.firebaseio.com",
+    projectId: "o-shop-8fc2f",
+    storageBucket: "",
+    messagingSenderId: "407124319642"
+  }
 };
